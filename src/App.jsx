7deca15a4dcc8b69de83883a9f3ef7aacdf83375
Cosmic-Pegasus <webdevelopercode24@gmail.com>
2024-05-08 22:23:46 +0530

    Nav changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
+
+import Nav from "./components/navbar/Nav";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar />
+      <Nav />
       <Home />
       <div style={{ height: "100vH" }}></div>
     </>
